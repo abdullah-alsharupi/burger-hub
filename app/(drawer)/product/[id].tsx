0b21273f1fac07cd81c55826ee9147c6c0997e0a
alisaleh22 <abdullahalsharupi@gmail.com>
2025-01-28 17:36:18 +0300

    forth commit

--- app/(drawer)/product/[id].tsx
+++ app/(drawer)/product/[id].tsx
@@ -4,7 +4,7 @@ import { router, Stack } from "expo-router";
 import { HeaderBackButton } from "@react-navigation/elements";
 import Header from "@/src/components/ui/Header";
 import AddproductsFavorite from "@/src/components/product/details/AddProductFavorite";
-import { ProductsDetailsScreen } from "@/src/components/product/details/ProductDetailsScreen";
+import { ProductDetailsScreen } from "@/src/components/product/details/ProductDetailsScreen";
 
 const ProductsDetails = () => {
   return (
@@ -15,7 +15,7 @@ const ProductsDetails = () => {
         headerRight={<AddproductsFavorite />}
       />
 
-      <ProductsDetailsScreen />
+      <ProductDetailsScreen />
     </>
   );
 };
